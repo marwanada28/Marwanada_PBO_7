0919b8d21ab2bc599d32495d4aa7fdb211e01f9a
nda <147893520+marwanada28@users.noreply.github.com>
2025-05-05 07:25:57 +0700

    petmanager.java

---
+++ petmanager.java
@@ -0,0 +1,21 @@
+##marwa nada
+package Service;
+
+import Model.Animal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class petmanager<T extends Animal> {
+    private List<T> pets = new ArrayList<>();
+
+    public void addPet(T pet) {
+        pets.add(pet);
+    }
+
+    public void listPets() {
+        for (T pet : pets) {
+            System.out.println("ID: " + pet.getId() + ", Name: " + pet.getName());
+            pet.speak();
+        }
+    }
+}
